018644508937788f3036439e451e26873b4ad94e
Tehreem Anwar <tehreemhb@gmail.com>
2026-08-08 17:07:19 -0400

    Update init_db.py

--- backend/app/database/init_db.py
+++ backend/app/database/init_db.py
@@ -1,1 +1,11 @@
 
+"""
+Create database tables.
+"""
+
+from app.database.database import Base, engine
+from app.database import models  # noqa: F401
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
